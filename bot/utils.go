34749dd12a609444b2750edf09ba46c62d929b22
okseby <okseby@protonmail.com>
2023-08-30 22:54:40 -0400

    Remove logging as it should be fixed now

--- bot/utils.go
+++ bot/utils.go
@@ -86,8 +86,6 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 		userType := strings.ToLower(event.GetComment().GetUser().GetType())
 		commentBody := event.GetComment().GetBody()
 
-		log.Println("User type:", userType, "Comment body:", commentBody)
-
 		if !strings.Contains(userType, "bot") && (strings.Contains(commentBody, "+1") || strings.Contains(commentBody, "👍")) {
 			log.Println("Received Issue Comment Event: processing now!")
 			processIssueCommentEvent(event)
