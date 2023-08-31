9513ba8897cbddf97520964545cfd4e79bd4bc64
okseby <okseby@protonmail.com>
2023-08-30 20:02:34 -0400

    Only merge if the comment body contains a thumbs up emoji

--- bot/utils.go
+++ bot/utils.go
@@ -86,7 +86,7 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 		eventLogin := event.GetComment().GetUser().GetLogin()
 		commentBody := event.GetComment().GetBody()
 
-		if !strings.Contains(eventLogin, "bot") && strings.Contains(commentBody, "+1") {
+		if !strings.Contains(eventLogin, "bot") && strings.Contains(commentBody, ":+1:") {
 			log.Println("Received Issue Comment Event: processing now!")
 			processIssueCommentEvent(event)
 			break
@@ -142,8 +142,9 @@ func processIssueCommentEvent(event *github.IssueCommentEvent) {
 		// Check if there are thumbs up (:+1:) reactions
 		for _, comment := range comments {
 			commentAuthor := comment.GetUser().GetLogin()
+			commentBody := comment.GetBody()
 
-			if !strings.Contains(commentAuthor, "bot") {
+			if !strings.Contains(commentAuthor, "bot") && strings.Contains(commentBody, ":+1:") {
 				_, exists := approvals[commentAuthor]
 				if !exists {
 					approvals[commentAuthor] = 1
